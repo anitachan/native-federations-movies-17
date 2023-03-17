eafa34ef2b72c27e52b7a8ce189e3a94045393eb
Ana Lucia Chavez <anlchave@bancolombia.com.co>
2023-03-17 11:41:24 -0500

    Change to jest all projects and fixing error for import shared library

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,27 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig");
+
 module.exports = {
-  moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
-  },
+  // moduleNameMapper: {
+  //   '@core/(.*)': '<rootDir>/src/$1',
+  // },
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  // roots: ["<rootDir>/src/"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: "<rootDir>/../../",
+  }),
+ // rootDir: '../'
+  //moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' } )
+  // moduleNameMapper: {
+  //   '^shared-lib$': '/Users/analuciachavezhidalgo/Documents/Anita/mfe-angular-federation/projects/shared-lib/src',
+  // },
+//   "moduleNameMapper": {
+//     "/@app1\/shared-lib/": "projects/app1/src/app/shared-lib"
+//  },
+//  rootDir: '..',
+//   moduleNameMapper: {
+//     //'@app1/shared-lib': '/Users/analuciachavezhidalgo/Documents/Anita/mfe-angular-federation/projects/shared-lib/src/public-api.ts',
+//     '@app1/shared-lib': '<rootDir>/../shared-lib/src/public-api.ts',
+//   },
 };
